fc363adcc4697d62d747e75cac2ee764b610f7f3
pradhinataraj <pradhee21@gmail.com>
2019-05-16 13:08:55 +0530

    java project

---
+++ src/org/add/GreensTech.java
@@ -0,0 +1,22 @@
+package org.add;
+
+public class GreensTech {
+public void greensOmr() {
+	System.out.println("Branch");
+}
+public void greensAdayar() {
+	System.out.println("Branch 2");
+
+}
+public void greensTambaram() {
+	System.out.println("Branch3");
+
+}
+public static void main(String[] args) {
+	GreensTech g=new GreensTech();
+	g.greensOmr();
+	g.greensAdayar();
+	g.greensTambaram();
+	
+}
+}
